607ce008208757e8e1fd744b1780fca1d73d75fb
Rui Wang <rwa56@lib7000-rcmac03.lib.sfu.ca>
2017-01-12 11:50:43 -0800

    third

--- ffmpeg_para/test5.sh
+++ ffmpeg_para/test5.sh
@@ -2,8 +2,8 @@
 
 for nb in 0 2 4 8
 do
-    ffmpeg -i KristenAndSara_720p.y4m -c:v libx264 -pix_fmt yuv420p -an -qp 20 -g 250 -keyint_min 250 -refs 2 -bf $nb Kris_ref_$nb.mp4
-    ffmpeg -i Stockholm_720p.y4m  -c:v libx264 -pix_fmt yuv420p -an -qp 20 -g 250 -keyint_min 250 -refs 2 -bf $nb Stoc_ref_$nb.mp4
+    ffmpeg -i KristenAndSara_720p.y4m -c:v libx264 -pix_fmt yuv420p -an -qp 20 -g 250 -keyint_min 250 -refs 2 -bf $nb Kris_nb_$nb.mp4
+    ffmpeg -i Stockholm_720p.y4m  -c:v libx264 -pix_fmt yuv420p -an -qp 20 -g 250 -keyint_min 250 -refs 2 -bf $nb Stoc_nb_$nb.mp4
 done
 
 
--- ffmpeg_para/test6.sh
+++ ffmpeg_para/test6.sh
@@ -3,8 +3,8 @@
 for qp in  30 40 50
 do
 
-    ffmpeg -i KristenAndSara_720p.y4m -c:v libx265 -pix_fmt yuv420p –an  -x265-params -qp $qp Kris_265_$qp.mp4
-    ffmpeg -i Stockholm_720p.y4m  -c:v libx265 -pix_fmt yuv420p -an -x265-params -qp $qp Stoc_265_$qp.mp4
+    ffmpeg -i KristenAndSara_720p.y4m -c:v libx265 -pix_fmt yuv420p -an -x265-params qp=$qp Kris_265_$qp.mp4
+    ffmpeg -i Stockholm_720p.y4m  -c:v libx265 -pix_fmt yuv420p -an -x265-params qp=$qp Stoc_265_$qp.mp4
 done
 
 
